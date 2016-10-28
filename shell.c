0162199988035fed1191728ebed95daaae4887f5
DavlukaAgMo <Davluka_D@hotmail.com>
2016-10-28 14:34:14 -0500

    shell con almacenamiento de cadena

--- shell.c
+++ shell.c
@@ -1,21 +1,49 @@
-#include<stdio.h>
-
-void clearBuffer();
-void hola();
-
-int main(void)
-{
-// El shell va a ser un programa que repetirá un bucle infinito. Se terminará 
-// Con ctrl + c
-char a;
-int b;  
-	while(b)
+#include <stdio.h>
+#include <stdlib.h>
+int obtenercadena(char** K)
+{// la funcion retornara el numero de caracteres leidos, utilizara la variable char que ya se definio en main y tambien almacenara espacio de memoria de manera dinamica del tamaño de la cadena ingresada
+	char a=getchar();
+	int m=0;//contador de caracteres de la cadena
+	//int n=0;//contador que sirve para almacenar de manera dinamica la memoria
+	if(a=='\n')
 	{
-		printf(">_ ");
-		scanf("%c",&a);
-		clearBuffer();
+		return 0;
+	}else
+	{
+		K[0]=(char*)calloc(1,sizeof(char));
+		K[0][0]=a;
+		++m;
+		do
+		{	
+			a=getchar();
+			if(a=='\n')
+			{
+				break;
+			}else
+			{
+			K[m]=(char*)calloc(1,sizeof(char));
+			K[0][m]=a;
+			++m;
+			}	
+		}while(a!='\n');
+		K[m]=(char*)calloc(1,sizeof(char));
+		K[0][m]='\0';
+		fflush(stdout);
+		return m;
 	}
+}
+int main(int argc, char const *argv[])
+{
+	int i;
+	char* k;
+	int p=0;
 
+	while(i)
+	{
+		printf("\n>_");
+		p=obtenercadena(&k);
+		printf("%s %d",k,p);
+	}
 	return 0;
 }
 
@@ -24,4 +52,5 @@ int b;
 void clearBuffer(void)
 {
 	while(getchar() != '\n');
+}
 }
