9effb2f2a3b381452e963b6f15d94ce8350affeb
Josemjr <jm_arco@hotmail.com>
2016-11-05 04:46:07 -0600

    Programa con pipes prueba

--- exe2.c
+++ exe2.c
@@ -8,10 +8,10 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <time.h>
+#include <signal.h>
 
 
-
-char prompt[5]={":v \0"};
+char prompt[5]={":v_\0"};
 char buf[1024]={'\0'};
 int salidatoarchivo=0;//servira para saber cuando desviar la salida estandar a un archivo
 void cambiaprompt1(int g)
@@ -56,11 +56,13 @@ void *toarchivo(void* arg)
 }
 
 int main()
-{       int mite=0,contadordeargumentos=0;
+{       int mite=0,contadordeargumentos=0,valor=1,comandos=0,i=0;
 	pthread_t mi_hilo;
 	char auxbuf[1023]={'\0'};
 	char *args[64];
 	char *args2[64];
+	char *comm[64];
+	fflush(stdin);
 	printf("%d\n",getpid());
 	for (;;) 
 	{
@@ -101,22 +103,58 @@ int main()
 			*/
                         
 			contadordeargumentos=divide(buf, args);
-         
+               
+		
 			/*
 			* Ejecutar el comando.
 			*/
-		        ejecutar(args);
+		        
+
+
+
+           		while (args[i] != NULL){
+
+
+			if(strcmp(args[i],"+")==0 && strcmp(args[i+1],"mite")==0 ){ // no queda :(
+                               args2[0]=args[i+2];
+				ejecutarpipe(args);
+				ejecutarmite(args2);
+					main();
+							 }
+
+
+
+			else if (strcmp(args[i],"+") == 0){  // compara caracter + para ejecutar pipe
+				ejecutarpipe(args);
+					main ();}
+		
+						
+
+                          else{
+		
+				ejecutar(args);
 			
-			// ejecutar tee 
+			     }
+
+                        					
 			
-			for(int i=0;i<=contadordeargumentos;i++)// viendo si hay "mite"(tee)
-                        { 
-                           if(strcmp(args[i],"mite")==0){
-                              args2[0]=args[i+1];
-			      ejecutarmite(args2);           //si existe, va a la funcion ejecutarmite
-            
-                                                        }  
-                        }
+
+
+
+
+			
+
+			i++;
+			}
+
+			
+          
+
+
+             
+                        
+
+			
 		}
 	}
 }
@@ -158,13 +196,13 @@ int divide(char *buf, char **args)
 * ejecutar--genera un proceso hijo y ejecuta
 *           el programa.
 */
-ejecutar(char **args)
+int ejecutar(char **args)
 {
-	int pid, status;
-	
-	    if(strcmp(*args,"mite")==0)//si el argumento es "mite" regresa para que lo ejecute la funcion ejecutarmite
-	    return;
+	int pid, status,valor=1;
 
+
+         if(strcmp(*args,"mite")==0)
+			return;
 	/*
 	* Obtener un proceso hijo.
 	*/
@@ -184,8 +222,9 @@ ejecutar(char **args)
 	*/
 	if (pid == 0) 
 	{
-		execvp(*args, args);
+		valor=execvp(*args, args);
 		perror(*args);
+			return valor;
 		exit(1);
 
 	/* NOTA: las versiones execv() y execvp() de execl() son utiles cuando
@@ -207,6 +246,7 @@ ejecutar(char **args)
 	if(pid!=0)
 	{
 		waitpid(pid,&status,'\0');
+	
 		/* vacio */ ;
 	}
 }
@@ -246,13 +286,22 @@ ejecutarmite(char **args)
 				write(ps[1],buffer,numbyte);
 			}		
 		      }
-	}
+		  close(ps[1]);
+	          close(pe[0]);
+                  fclose(archivo);
 
+             
+		exit(1);
+
+
+	}
+        
 
 	if(pid!=0)
 	{
-		           
+	
 		waitpid(pid,&status,'\0');
+
 	
 		/* vacio */ ;
 	}
@@ -260,8 +309,164 @@ ejecutarmite(char **args)
 
 
 
-  
+ void ejecutarpipe(char * args[]){
+	// File descriptors
+	int filedes[2]; // pos. 0 output, pos. 1 input of the pipe
+	int filedes2[2];
 	
+	int num_cmds = 0;
+	
+	char *command[256];
+	
+	pid_t pid;
+	
+	int err = -1;
+	int end = 0;
+	
+	// Variables used for the different loops
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int l = 0;
+	
+	// First we calculate the number of commands (they are separated
+	// by '|')
+	while (args[l] != NULL){
+		if (strcmp(args[l],"+") == 0){
+			num_cmds++;
+		}
+		l++;
+	}
+	num_cmds++;
+	
+	// Main loop of this method. For each command between '|', the
+	// pipes will be configured and standard input and/or output will
+	// be replaced. Then it will be executed
+	while (args[j] != NULL && end != 1){
+		k = 0;
+		// We use an auxiliary array of pointers to store the command
+		// that will be executed on each iteration
+		while (strcmp(args[j],"+") != 0){
+			command[k] = args[j];
+			j++;	
+			if (args[j] == NULL){
+				// 'end' variable used to keep the program from entering
+				// again in the loop when no more arguments are found
+				end = 1;
+				k++;
+				break;
+			}
+			k++;
+		}
+		// Last position of the command will be NULL to indicate that
+		// it is its end when we pass it to the exec function
+		command[k] = NULL;
+		j++;		
+		
+		// Depending on whether we are in an iteration or another, we
+		// will set different descriptors for the pipes inputs and
+		// output. This way, a pipe will be shared between each two
+		// iterations, enabling us to connect the inputs and outputs of
+		// the two different commands.
+		if (i % 2 != 0){
+			pipe(filedes); // for odd i
+		}else{
+			pipe(filedes2); // for even i
+		}
+		
+		pid=fork();
+		
+		if(pid==-1){			
+			if (i != num_cmds - 1){
+				if (i % 2 != 0){
+					close(filedes[1]); // for odd i
+				}else{
+					close(filedes2[1]); // for even i
+				} 
+			}			
+			printf("Child process could not be created\n");
+			return;
+		}
+		if(pid==0){
+			// If we are in the first command
+			if (i == 0){
+				dup2(filedes2[1], STDOUT_FILENO);
+			}
+			// If we are in the last command, depending on whether it
+			// is placed in an odd or even position, we will replace
+			// the standard input for one pipe or another. The standard
+			// output will be untouched because we want to see the 
+			// output in the terminal
+			else if (i == num_cmds - 1){
+				if (num_cmds % 2 != 0){ // for odd number of commands
+					dup2(filedes[0],STDIN_FILENO);
+				}else{ // for even number of commands
+					dup2(filedes2[0],STDIN_FILENO);
+				}
+			// If we are in a command that is in the middle, we will
+			// have to use two pipes, one for input and another for
+			// output. The position is also important in order to choose
+			// which file descriptor corresponds to each input/output
+			}else{ // for odd i
+				if (i % 2 != 0){
+					dup2(filedes2[0],STDIN_FILENO); 
+					dup2(filedes[1],STDOUT_FILENO);
+				}else{ // for even i
+					dup2(filedes[0],STDIN_FILENO); 
+					dup2(filedes2[1],STDOUT_FILENO);					
+				} 
+			}
+			
+			if (execvp(command[0],command)==err){
+				kill(getpid(),SIGTERM);
+		
+			}		
+		}
+				
+		// CLOSING DESCRIPTORS ON PARENT
+		if (i == 0){
+			close(filedes2[1]);
+		}
+		else if (i == num_cmds - 1){
+			if (num_cmds % 2 != 0){					
+				close(filedes[0]);
+			}else{					
+				close(filedes2[0]);
+			}
+		}else{
+			if (i % 2 != 0){					
+				close(filedes2[0]);
+				close(filedes[1]);
+			}else{					
+				close(filedes[0]);
+				close(filedes2[1]);
+			}
+		}
+				
+		waitpid(pid,NULL,0);
+				
+		i++;	
+	}
+} 
+	
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
